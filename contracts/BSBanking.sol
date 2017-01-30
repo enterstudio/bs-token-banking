24511628312d5ed167275279b4ce5e27dbd0fddb
Jose Luis Piedrahita <jlpiedrahita@gmail.com>
2017-01-30 13:51:12 +0100

    Add BSBanking contract

---
+++ contracts/BSBanking.sol
@@ -0,0 +1,26 @@
+pragma solidity ^0.4.8;
+
+import "Ownable.sol";
+import "BSTokenData.sol";
+
+contract BSBanking is Ownable {
+
+    BSTokenData public tokenData;
+    event CashOut(address indexed receiver, uint256 amount, string bankAccount);
+
+    function BSBanking(address bsTokenDataAddress) {
+        tokenData = BSTokenData(bsTokenDataAddress);
+    }
+
+    function cashOut(address sender, uint256 amount, string bankAccount) onlyOwner {
+        tokenData.setBalance(sender, tokenData.getBalance(sender) - amount);
+        tokenData.setTotalSupply(tokenData.getTotalSupply() - amount);
+        CashOut(sender, amount, bankAccount);
+    }
+
+    function cashIn(address sender, uint256 amount) onlyOwner {
+        tokenData.setBalance(sender, tokenData.getBalance(sender) + amount);
+        tokenData.setTotalSupply(tokenData.getTotalSupply() - amount);
+    }
+
+}
